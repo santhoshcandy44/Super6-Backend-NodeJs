07d7a4ae7e983b346777daf234fe032ec04b7325
santhoshcandy44 <santhoshcandy44@gmail.com>
2025-09-27 12:10:58 +0000

    bug fixes

--- controllers/jobsProtectedController.js
+++ controllers/jobsProtectedController.js
@@ -425,7 +425,6 @@ exports.updateSkill = async (req, res) => {
             next_complete_step: getNextIncompleteStep(result)
         });
     } catch (error) {
-        console.log(error)
         return sendErrorResponse(res, 500, "Internal Server Error", error.message);
     }
 };
@@ -456,11 +455,57 @@ exports.updateLanguage = async (req, res) => {
                 intro: result.intro,
                 profile_pic_url: result.profile_picture
             },
-            applicant_education: result.educationList,
-            applicant_experience: result.experienceList, 
-            applicant_skill: result.skillsList,
-            applicant_language: result.languagesList,
-            applicant_certificate: result.certificateList,
+            applicant_educations: result.educationList,
+            applicant_experiences: result.experienceList, 
+            applicant_skills: result.skillsList,
+            applicant_languages: result.languagesList,
+            applicant_certificates: result.certificateList,
+            applicant_resume: result.resume ? {
+                resume: result.resume.resume_download_url,
+                file_name: result.resume.file_name,
+                file_size: result.resume.resume_size,
+                type: result.resume.resume_type,
+                last_used: result.resume.last_used
+            } : null,
+            next_complete_step: getNextIncompleteStep(result)
+        });
+    } catch (error) {
+        return sendErrorResponse(res, 500, "Internal Server Error", error.message);
+    }
+};
+
+exports.updateResume = async (req, res) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            const firstError = errors.array()[0]; 
+            return sendErrorResponse(res, 400, firstError.msg, errors.array());
+        }
+        const applicantResumeInfoJson = req.body.applicantResumeInfo;
+        if (!applicantResumeInfoJson) {
+            return sendErrorResponse(res, 400, "Missing resume info part");
+        }
+        const userId = req.user.user_id;
+        const applicantResumeInfo = JSON.parse(applicantResumeInfoJson);
+        const resume = req.file;
+        const result = await ApplicantProfile.updateOrCreateUserResume(userId, resume);
+        if (!result) {
+            return sendErrorResponse(res, 400, "Failed to update resume");
+        }
+        return sendJsonResponse(res, 200, "Resume info updated successfully", {
+            applicant_professional_info: {
+                first_name: result.first_name,
+                last_name: result.last_name,
+                email: result.email,
+                gender: result.gender,
+                intro: result.intro,
+                profile_pic_url: result.profile_picture
+            },
+            applicant_educations: result.educationList,
+            applicant_experiences: result.experienceList,
+            applicant_skills: result.skillsList,
+            applicant_languages: result.languagesList,
+            applicant_certificates: result.certificateList,
             applicant_resume: result.resume ? {
                 resume: result.resume.resume_download_url,
                 file_name: result.resume.file_name,
@@ -526,11 +571,11 @@ exports.updateCertificate = async (req, res) => {
                 intro: result.intro,
                 profile_pic_url: result.profile_picture
             },
-            applicant_education: result.educationList,
-            applicant_experience: result.experienceList,
-            applicant_skill: result.skillsList,
-            applicant_language: result.languagesList,
-            applicant_certificate: result.certificateList,
+            applicant_educations: result.educationList,
+            applicant_experiences: result.experienceList,
+            applicant_skills: result.skillsList,
+            applicant_languages: result.languagesList,
+            applicant_certificates: result.certificateList,
             applicant_resume: result.resume ? {
                 resume: result.resume.resume_download_url,
                 file_name: result.resume.file_name,
@@ -542,52 +587,6 @@ exports.updateCertificate = async (req, res) => {
         });
     } catch (error) {
         console.error(error);
-        return sendErrorResponse(res, 500, "Internal Server Error", error.message);
-    }
-};
-
-exports.updateResume = async (req, res) => {
-    try {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            const firstError = errors.array()[0]; 
-            return sendErrorResponse(res, 400, firstError.msg, errors.array());
-        }
-        const applicantResumeInfoJson = req.body.applicantResumeInfo;
-        if (!applicantResumeInfoJson) {
-            return sendErrorResponse(res, 400, "Missing resume info part");
-        }
-        const userId = req.user.user_id;
-        const applicantResumeInfo = JSON.parse(applicantResumeInfoJson);
-        const resume = req.file;
-        const result = await ApplicantProfile.updateOrCreateUserResume(userId, resume);
-        if (!result) {
-            return sendErrorResponse(res, 400, "Failed to update resume");
-        }
-        return sendJsonResponse(res, 200, "Resume info updated successfully", {
-            applicant_professional_info: {
-                first_name: result.first_name,
-                last_name: result.last_name,
-                email: result.email,
-                gender: result.gender,
-                intro: result.intro,
-                profile_pic_url: result.profile_picture
-            },
-            applicant_education: result.educationList,
-            applicant_experience: result.experienceList,
-            applicant_skill: result.skillsList,
-            applicant_language: result.languagesList,
-            applicant_certificate: result.certificateList,
-            applicant_resume: result.resume ? {
-                resume: result.resume.resume_download_url,
-                file_name: result.resume.file_name,
-                file_size: result.resume.resume_size,
-                type: result.resume.resume_type,
-                last_used: result.resume.last_used
-            } : null,
-            next_complete_step: getNextIncompleteStep(result)
-        });
-    } catch (error) {
         return sendErrorResponse(res, 500, "Internal Server Error", error.message);
     }
 };
