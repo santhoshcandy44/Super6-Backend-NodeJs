bb11f26ebffc8cfcbdfffee27c3673f78e078804
santhoshcandy44 <santhoshcandy44@gmail.com>
2025-10-07 12:18:38 +0000

    bug fixes

--- models/UsedProdctListing.js
+++ models/UsedProdctListing.js
@@ -476,7 +476,7 @@ WHERE
                 if (radius < 200) {
                     radius += 30;
                     await connection.release();
-                    return await this.getUsedProductListingsForUser(userId, queryParam, 1, pageSize, lastTimeStamp, lastTotalRelevance, radius)
+                    return await this.getUsedProductListingsForUser(userId, queryParam, afterId, pageSize, lastTimeStamp, lastTotalRelevance, radius)
                 }
             }
         }
