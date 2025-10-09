3b4457b5eb0ebcfd4bfc7f9e63f5ea0396a216d6
santhoshcandy44 <santhoshcandy44@gmail.com>
2025-10-09 08:30:22 +0000

    bug fixes

--- controllers/localJobsProtectedController.js
+++ controllers/localJobsProtectedController.js
@@ -118,6 +118,7 @@ exports.getLocalJobApplications = async (req, res) => {
         }
         return sendJsonResponse(res, 200, "Local job applicants retrieved successfully", result);
     } catch (error) {
+        console.log(error);
         return sendErrorResponse(res, 500, "Internal Server Error", error.message);
     }
 };
