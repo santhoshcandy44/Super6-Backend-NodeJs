35472b7655a174cc8fc5331874f4bee6c1b3c735
santhoshcandy44 <santhoshcandy44@gmail.com>
2025-10-10 12:27:11 +0000

    bug fixes

--- models/Job.js
+++ models/Job.js
@@ -354,12 +354,13 @@ CASE WHEN a.applicant_id IS NOT NULL THEN TRUE ELSE FALSE END AS is_applied,
 
         if (payload) {
           query += ` AND (
-                 (j.id > ?)
+                 (j.posted_at = ? AND j.id > ?)
               )
           `;
 
           params.push(
           
+            payload.posted_at,
             payload.id
           );
 
