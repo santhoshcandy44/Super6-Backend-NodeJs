3a39da9067eb613a08b5dde8a579436583da4181
santhoshcandy44 <santhoshcandy44@gmail.com>
2025-10-10 10:54:07 +0000

    bug fixes

--- controllers/servicesProtectedController.js
+++ controllers/servicesProtectedController.js
@@ -29,7 +29,7 @@ exports.getServices = async (req, res) => {
         const queryNextToken = !next_token ? null : next_token;
         const decodedQuery = decodeURIComponent(querySearch.replace(/\+/g, ' '));
         const PAGE_SIZE = page_size ? page_size : 20;
-        const result = await Service.getServices(user_id, decodedQuery, 1, queryNextToken);
+        const result = await Service.getServices(user_id, decodedQuery, PAGE_SIZE, queryNextToken);
         if (!result) {
             return sendErrorResponse(res, 400, "Failed to retrieve services");
         }
@@ -132,7 +132,7 @@ exports.getPublishedServices = async (req, res) => {
         const {page_size, next_token} = req.query;
         const queryNextToken = next_token ? next_token : null;
         const PAGE_SIZE = page_size ? page_size : 20;
-        const result = await Service.getUserPublishedServices(user_id, PAGE_SIZE, queryNextToken)
+        const result = await Service.getUserPublishedServices(user_id, 1, queryNextToken)
         if (!result) {
             return sendErrorResponse(res, 400, "Failed to retrieve services");
         }
