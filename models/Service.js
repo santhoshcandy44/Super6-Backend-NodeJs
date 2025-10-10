4a2f89df085f780ddf5db84cb3b055a4642d8012
santhoshcandy44 <santhoshcandy44@gmail.com>
2025-10-10 08:28:18 +0000

    bug  fixes

--- models/Service.js
+++ models/Service.js
@@ -371,7 +371,6 @@ WHERE
                     params.push(
                         payload.distance,
                         payload.distance,
-                        payload.distance,
                         payload.created_at,
                         payload.distance,
                         payload.created_at,
