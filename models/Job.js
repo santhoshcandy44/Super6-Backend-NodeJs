57e0da935c18c8e906ff2d3b693d7bd2db27d80f
santhoshcandy44 <santhoshcandy44@gmail.com>
2025-10-10 11:18:48 +0000

    bug fixes

--- models/Job.js
+++ models/Job.js
@@ -819,6 +819,7 @@ CASE WHEN a.applicant_id IS NOT NULL THEN TRUE ELSE FALSE END AS is_applied,
         id: lastItem.id
     } : null;
 
+    console.log(lastItem);
     return {
         data: allItems,
         next_token: payloadToEncode ? encodeCursor(
