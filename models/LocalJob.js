a9e79c0f151bcafda7869030339d7f65550c8731
santhoshcandy44 <santhoshcandy44@gmail.com>
2025-10-11 08:05:08 +0000

    bug fixes

--- models/LocalJob.js
+++ models/LocalJob.js
@@ -707,7 +707,9 @@ WHERE
         var radius = initialRadius;
         const payload = nextToken ? decodeCursor(nextToken) : null;
 
-        if (userCoordsData && userCoordsData.latitude && userCoordsData.longitude) {
+        // if (userCoordsData && userCoordsData.latitude && userCoordsData.longitude) {
+            if (false) {
+
             const userLat = userCoordsData.latitude;
             const userLon = userCoordsData.longitude;
 
