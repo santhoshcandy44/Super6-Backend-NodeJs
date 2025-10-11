367a97af00714d3bbe0aea20fdceeaef198134b1
santhoshcandy44 <santhoshcandy44@gmail.com>
2025-10-11 03:43:08 +0000

    bug fixes

--- controllers/usedProductsProtectedController.js
+++ controllers/usedProductsProtectedController.js
@@ -61,7 +61,7 @@ exports.getFeedUserPublishedUsedProductListings = async (req, res) => {
         const { page_size, next_token } = req.query;
         const queryNextToken = next_token ? next_token : null;
         const PAGE_SIZE = page_size ? page_size : 20;
-        const result = await UsedProductListing.getPublishedUsedProductListings(user_id, PAGE_SIZE, queryNextToken)
+        const result = await UsedProductListing.getPublishedUsedProductListings(user_id, 1, queryNextToken)
         if (!result) {
             return sendErrorResponse(res, 400, "Failed to retrieve used product listings");
         }
