35816e726fbbf1405a05eb5ba088e0a1f1853618
santhoshcandy44 <santhoshcandy44@gmail.com>
2025-10-12 13:32:06 +0000

    bug fixes

--- controllers/jobsProtectedController.js
+++ controllers/jobsProtectedController.js
@@ -220,7 +220,6 @@ exports.searchSkillsSuggestions = async (req, res) => {
             { skill: "Kotlin", skill_code: "KT" },
             { skill: "Python", skill_code: "PY" }
           ];
-          
         return sendJsonResponse(res, 200, "Suggestions retrieved successfully", skills);
     } catch (error) {
         return sendErrorResponse(res, 500, "Internal Server Error", error.toString());
