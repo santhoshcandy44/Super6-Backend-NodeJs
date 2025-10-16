88c602e39394243c733f2a0e559fc0df98ef03b6
santhoshcandy44 <santhoshcandy44@gmail.com>
2025-10-16 05:51:12 +0000

    bug fixes

--- routes/localJobsProtectedAppRoutes.js
+++ routes/localJobsProtectedAppRoutes.js
@@ -2,12 +2,10 @@ const express = require('express');
 const { body, param, query } = require('express-validator');
 const authenticateToken = require('../middlewares/authMiddleware');
 const he = require('he');
-const multer = require('multer');
 const localJobsProtectedController = require('../controllers/localJobsProtectedController');
 const { uploadMultiple } = require('./utils/multerUpload');
 
 const router = express.Router();
-const upload = multer();
 
 router.get('/local-jobs',
     authenticateToken,
