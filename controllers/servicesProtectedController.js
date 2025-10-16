ea3d90d55cd834d7a960540aece0c1e6c0773170
santhoshcandy44 <santhoshcandy44@gmail.com>
2025-10-16 10:18:38 +0000

    bug fixes

--- controllers/servicesProtectedController.js
+++ controllers/servicesProtectedController.js
@@ -34,7 +34,7 @@ exports.getServices = async (req, res) => {
         if (!result) {
             return sendErrorResponse(res, 400, "Failed to retrieve services");
         }
-        return sendJsonResponse(res, 200, "Services retrieved successfully", result);
+        return sendJsonResponse(res, 200, "Services retrieved successfully", []);
     } catch (error) {
         console.log(error)
         return sendErrorResponse(res, 500, "Internal Server Error", error.message);
